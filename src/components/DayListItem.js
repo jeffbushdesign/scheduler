a5ff5057d4de4c062686ffca59987d95ec43875e
Jeff Bush <jeffbushdesign@gmail.com>
2022-05-21 16:35:47 -0400

    Test the DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,6 +5,21 @@ import "components/Application.scss";
 import classNames from "classnames";
 
 export default function DayListItem(props) {
+  let message;
+  if (props.spots === 0) {
+    message = (
+      <h3 className="text--light">no spots remaining</h3>
+    );
+  } else if (props.spots === 1) {
+    message = (
+      <h3 className="text--light">1 spot remaining</h3>
+    );
+  } else {
+    message = (
+      <h3 className="text--light">{props.spots} spots remaining</h3>
+    );
+  }
+
   const btnClass = classNames({
     'day-list__item': true,
     'day-list__item--selected': props.selected,
@@ -13,7 +28,7 @@ export default function DayListItem(props) {
   return (
     <li className={btnClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      {message}
     </li>
   );
 }
