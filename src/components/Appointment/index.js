f3a1cbe2129ac980bcb5db991b1f697a1d8d9066
Jeff Bush <jeffbushdesign@gmail.com>
2022-05-23 13:42:17 -0400

    Add edit and delete to Show stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,8 @@ import "./styles.scss";
 
 export default function Appointment(props) {
   return (
-    <article className='appointment'></article>
+    <article className='appointment'>
+
+    </article>
   );
 }
