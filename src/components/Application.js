d7eb42e921a6db3b1fed6bf0a443ebf271d2abae
Jeff Bush <jeffbushdesign@gmail.com>
2022-06-01 23:18:05 -0400

    Start Retrieving Interviewer Data exercise

--- src/components/Application.js
+++ src/components/Application.js
@@ -51,13 +51,20 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
 
+
+
+
+
   // we can create a function called setDay that updates the state with the new day
   const setDay = day => setState({ ...state, day });
+
+
 
 
 
@@ -82,9 +89,11 @@ export default function Application(props) {
     Promise.all([
       axios.get('api/days'),
       axios.get('api/appointments'),
+      axios.get('api/interviewers')
     ]).then((all) => {
       // console.log('days: all[0]', all[0], 'appointments: all[1]', all[1]);
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }));
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+      console.log('interviewers:', all[2].data);
 
 
 
