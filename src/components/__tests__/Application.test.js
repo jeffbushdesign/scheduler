9fa40e44fa670385d69759b64a17cd8b47e49551
Jeff Bush <jeffbushdesign@gmail.com>
2022-06-16 14:34:26 -0400

    Add integration testing

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,16 @@
 import React from "react";
+import axios from 'axios';
 import {
   render,
   cleanup,
   waitForElement,
   fireEvent,
   getByText,
-  prettyDOM
+  prettyDOM,
+  getAllByTestId,
+  getByAltText,
+  getByPlaceholderText
+
 } from "@testing-library/react";
 import Application from "components/Application";
 
@@ -23,8 +28,21 @@ describe("Application", () => {
 
   it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
     const { container } = render(<Application />);
-    await waitForElement(() => getByText(container, "Archie Cohen"));
-    console.log(prettyDOM(container));
-  });
 
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+
+    console.log(prettyDOM(appointment));
+  });
 });
